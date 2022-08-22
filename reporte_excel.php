9411bb687563800328a7fa5739e3b3a242187b08
luisjuarezsanchez <luisjuarezingreso@hotmail.com>
2022-08-22 14:16:06 -0500

    Reportes TXT y Excel funcionando

--- reporte_excel.php
+++ reporte_excel.php
@@ -1,29 +1,60 @@
 <?php
+
+
 require 'conexion.php';
 require 'vendor/autoload.php';
 //Solicitando el objeto a la Biblioteca
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\IOFactory;
-use PhpOffice\PhpSpreadsheet\PHPExcel_Style_NumberFormat;
-use PhpOffice\PhpSpreadsheet\Style\Border;
-use PhpOffice\PhpSpreadsheet\Style\Color;
 use PhpOffice\PhpSpreadsheet\Style\Fill;
 
-$TableHead = [
-  'font'=>[
-    'color'=>[
-      'rgb'=>'FFFFFF'
+
+$encabezados = [
+  'borders' => array(
+    'vertical' => array(
+      'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THICK,
+      'color' => array('argb' => 'FFFFFF'),
+    ),
+  ),
+
+  'font' => [
+    'color' => [
+      'rgb' => 'FFFFFF'
     ],
-    'bold=' => true,
-    'size'=>11
+    'bold' => true,
+    'size' => 11
   ],
-  'fill'=>[
+  'fill' => [
     'fillType' => Fill::FILL_SOLID,
-      'startColor'=>[
-        'rgb' => '538ED5'
-            ]  
-         ]
-      ];
+    'startColor' => [
+      'rgb' => '538ED5'
+    ]
+  ]
+  
+];
+
+
+$bordesH = [
+  'borders' => array(
+    'vertical' => array(
+      'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+      'color' => array('argb' => '000000'),
+    ),
+  ),
+];
+
+$bordesV = [
+  'borders' => array(
+    'outline' => array(
+      'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_DASHED,
+      'color' => array('argb' => '000000'),
+    ),
+  ),
+];
+
+//Efectuando la consulta SWL
+$consulta8 = "CALL sp_GeneraTmpSerPub('202212 10094')";
+$resultado8 = $mysqli->query($consulta8);
 
 //Efectuando la consulta SWL
 $consulta7 = "SELECT * FROM tmpSerPub";
@@ -38,41 +69,23 @@ $hojaActiva->setTitle("Empleados");
 
 //Asignando color a las celdas en un rango
 $excel->getActiveSheet()->getStyle('A1:L1')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
-->getStartColor()->setARGB('4472C4');
+  ->getStartColor()->setARGB('3678C7');
 
-$excel->getActiveSheet()->getStyle('B:L')->getNumberFormat()->
-setFormatCode(\PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_NUMBER_00);
+$excel->getActiveSheet()->getStyle('B:L')->getNumberFormat()->setFormatCode(\PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_NUMBER_00);
 
 
 //Alineacion
 //$excel->getActiveSheet() ->getStyle('A:L')->setQuotePrefix(true);
-$excel->getActiveSheet()->getStyle('A1:L1')->getAlignment()->setWrapText(true);
+$excel->getActiveSheet()->getStyle('A1:L1')->getAlignment()->setWrapText(true)->setVertical(true);
 
 $excel->getActiveSheet()->getStyle('A2:L2')
-    ->getAlignment()->setVertical(\PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_TOP);
+  ->getAlignment()->setVertical(\PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_TOP);
 
-  $excel->getActiveSheet()->getStyle('A1:L1')->ApplyFromArray($TableHead);
+$excel->getActiveSheet()->getStyle('A1:L1')->ApplyFromArray($encabezados);
+//$excel->getActiveSheet()->getStyle('A4:L4')->ApplyFromArray($bordes);
 
-
-
-
-
-
-
-
-
-//$excel->getActiveSheet()->getStyle('A2:L2')
-  //  ->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT);
-
-
-
-
-
-
-
-
-$hojaActiva->getColumnDimension('A')->setWidth(10);//Anchura de la celda
-$hojaActiva->setCellValue('A1', 'PERIODO');//Titulo de la columna
+$hojaActiva->getColumnDimension('A')->setWidth(10); //Anchura de la celda
+$hojaActiva->setCellValue('A1', 'PERIODO'); //Titulo de la columna
 $hojaActiva->getColumnDimension('B')->setWidth(19);
 $hojaActiva->setCellValue('B1', 'CLAVE UNIDAD ADMINISTRATIVA');
 $hojaActiva->getColumnDimension('C')->setWidth(15);
@@ -81,48 +94,61 @@ $hojaActiva->getColumnDimension('D')->setWidth(50);
 $hojaActiva->setCellValue('D1', 'NOMBRE');
 $hojaActiva->getColumnDimension('E')->setWidth(20);
 $hojaActiva->setCellValue('E1', 'RFC');
-$hojaActiva->getColumnDimension('F')->setWidth(10);
+$hojaActiva->getColumnDimension('F')->setWidth(13);
 $hojaActiva->setCellValue('F1', '0202 SUELDOS EVENTUALES');
-$hojaActiva->getColumnDimension('G')->setWidth(10);
+$hojaActiva->getColumnDimension('G')->setWidth(11);
 $hojaActiva->setCellValue('G1', '0325 SUBSIDIO AL EMPLEO');
-$hojaActiva->getColumnDimension('H')->setWidth(10);
+$hojaActiva->getColumnDimension('H')->setWidth(12);
 $hojaActiva->setCellValue('H1', '5408 IMPUESTO SOBRE LA RENTA');
-$hojaActiva->getColumnDimension('I')->setWidth(10);
+$hojaActiva->getColumnDimension('I')->setWidth(12);
 $hojaActiva->setCellValue('I1', '5540 SERVICIOS DE SALUD');
-$hojaActiva->getColumnDimension('J')->setWidth(10);
+$hojaActiva->getColumnDimension('J')->setWidth(11);
 $hojaActiva->setCellValue('J1', '5541 SIS. SOLIDARIO DE REPARTO');
-$hojaActiva->getColumnDimension('K')->setWidth(10);
+$hojaActiva->getColumnDimension('K')->setWidth(13);
 $hojaActiva->setCellValue('K1', '5542 SIS. CAPITAL. INDIVIDUAL');
 $hojaActiva->getColumnDimension('L')->setWidth(10);
 $hojaActiva->setCellValue('L1', 'TOTAL NETO');
 
 //Indicar que se comience desde la fila 2 de Excel y no reescribir los encanezados
 $fila = 2;
+$borde = 1;
 
 //Ciclo para leer el contenido de la consulta
 while ($rows = $resultado7->fetch_assoc()) {
-    //Extrayendo campos de la BD y especificando la columna donde se mostrara el contenido
-    $hojaActiva->setCellValue('A' . $fila, $rows['Periodo']);
-    $hojaActiva->setCellValue('B' . $fila, $rows['ClaveUniAds']);
-    $hojaActiva->setCellValue('C' . $fila, $rows['CvePersonal']);
-    $hojaActiva->setCellValue('D' . $fila, $rows['Nombre']);
-    $hojaActiva->setCellValue('E' . $fila, $rows['Rfc']);
-    $hojaActiva->setCellValue('F' . $fila, $rows['SuelEven']);
-    $hojaActiva->setCellValue('G' . $fila, $rows['SubsEmp']);
-    $hojaActiva->setCellValue('H' . $fila, $rows['Isr']);
-    $hojaActiva->setCellValue('I' . $fila, $rows['ServSalud']);
-    $hojaActiva->setCellValue('J' . $fila, $rows['SisSolRep']);
-    $hojaActiva->setCellValue('K' . $fila, $rows['SisCapInd']);
-    $hojaActiva->setCellValue('L' . $fila, $rows['TotNet']);
-    $fila++;//Incrementando las filas en 1 para que se inserten apropiadamente
+  //Extrayendo campos de la BD y especificando la columna donde se mostrara el contenido
+  $hojaActiva->setCellValue('A' . $fila, $rows['Periodo']);
+  $hojaActiva->setCellValue('B' . $fila, $rows['ClaveUniAds']);
+  $hojaActiva->setCellValue('C' . $fila, $rows['CvePersonal']);
+  $hojaActiva->setCellValue('D' . $fila, $rows['Nombre']);
+  $hojaActiva->setCellValue('E' . $fila, $rows['Rfc']);
+  $hojaActiva->setCellValue('F' . $fila, $rows['SuelEven']);
+  $hojaActiva->setCellValue('G' . $fila, $rows['SubsEmp']);
+  $hojaActiva->setCellValue('H' . $fila, $rows['Isr']);
+  $hojaActiva->setCellValue('I' . $fila, $rows['ServSalud']);
+  $hojaActiva->setCellValue('J' . $fila, $rows['SisSolRep']);
+  $hojaActiva->setCellValue('K' . $fila, $rows['SisCapInd']);
+  $hojaActiva->setCellValue('L' . $fila, $rows['TotNet']);
+  //Incrementando las filas en 1 para que se inserten apropiadamente
+  $fila++;
+  //Incrementando las filas en 1 para que el borde se pinte segun el numero de columnas
+  $borde++;
+  //Indicando las celdas en las que se generara el borde vertical
+  $excel->getActiveSheet()->getStyle("A$borde:M$borde")->ApplyFromArray($bordesH);
+  $excel->getActiveSheet()->getStyle("A$borde:L$borde")->ApplyFromArray($bordesV);
 }
 
 //Creando el archivo de excel
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-header('Content-Disposition: attachment;filename="Reporte_Empleados.xlsx"');
+header('Content-Disposition: attachment;filename="Reporte_Empleados.Xlsx"');
 header('Cache-Control: max-age=0');
 
 //Indicando la salida del documento en xlsx 
 $writer = IOFactory::createWriter($excel, 'Xlsx');
 $writer->save('php://output');
 exit;
+
+
+
+
+
+
--- reportes_txt.php
+++ reportes_txt.php
@@ -3,10 +3,11 @@ $CveNomina = $_POST['CveNomina'];
 $FecPag = $_POST['FecPag'];
 $num = 1;
 $a = 0;
-echo $CveNomina;
-echo "<br>";
-echo $FecPag;
 
+//Bibliotecas para reporte de Excel
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
 
 /********************Timbrado Maestro********************************** */
 //Abriendo el archivo en modo de escritura
@@ -14,6 +15,7 @@ $file = fopen("C:/Users/luisj/Desktop/MAESTRO.txt", "w");
 
 //Solicitando la conexion con la BD
 require 'conexion.php';
+require 'vendor/autoload.php';
 
 //Llamando al procedimiento que inserta los campos en tmptimmaes
 $consulta = "CALL sp_GeneraTmpTimMaes('$CveNomina')";
@@ -164,15 +166,55 @@ fclose($file2);
 
 
 
+$encabezados = [
+  'borders' => array(
+    'vertical' => array(
+      'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THICK,
+      'color' => array('argb' => 'FFFFFF'),
+    ),
+  ),
 
-require 'conexion.php';
-require 'vendor/autoload.php';
-//Solicitando el objeto a la Biblioteca
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\IOFactory;
+  'font' => [
+    'color' => [
+      'rgb' => 'FFFFFF'
+    ],
+    'bold' => true,
+    'size' => 11
+  ],
+  'fill' => [
+    'fillType' => Fill::FILL_SOLID,
+    'startColor' => [
+      'rgb' => '538ED5'
+    ]
+  ]
+  
+];
+
+
+$bordesH = [
+  'borders' => array(
+    'vertical' => array(
+      'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+      'color' => array('argb' => '000000'),
+    ),
+  ),
+];
+
+$bordesV = [
+  'borders' => array(
+    'outline' => array(
+      'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_DASHED,
+      'color' => array('argb' => '000000'),
+    ),
+  ),
+];
 
 //Efectuando la consulta SWL
-$consulta7 = "SELECT CvePersonal, RFC, Paterno, Materno, Nombre FROM EmpGral";
+$consulta8 = "CALL sp_GeneraTmpSerPub('$CveNomina')";
+$resultado8 = $mysqli->query($consulta8);
+
+//Efectuando la consulta SWL
+$consulta7 = "SELECT * FROM tmpSerPub";
 $resultado7 = $mysqli->query($consulta7);
 
 //Creando hoja de Excel
@@ -183,41 +225,87 @@ $hojaActiva = $excel->getActiveSheet();
 $hojaActiva->setTitle("Empleados");
 
 //Asignando color a las celdas en un rango
-$excel->getActiveSheet()->getStyle('A1:E1')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
-->getStartColor()->setARGB('00FF7F');
+$excel->getActiveSheet()->getStyle('A1:L1')->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)
+  ->getStartColor()->setARGB('3678C7');
 
-$hojaActiva->getColumnDimension('A')->setWidth(20);//Anchura de la celda
-$hojaActiva->setCellValue('A1', 'Periodo');//Titulo de la columna
-$hojaActiva->getColumnDimension('B')->setWidth(20);
-$hojaActiva->setCellValue('B1', 'RFC');
-$hojaActiva->getColumnDimension('C')->setWidth(20);
-$hojaActiva->setCellValue('C1', 'Paterno');
-$hojaActiva->getColumnDimension('D')->setWidth(20);
-$hojaActiva->setCellValue('D1', 'Materno');
-$hojaActiva->getColumnDimension('E')->setWidth(25);
-$hojaActiva->setCellValue('E1', 'Nombre');
+$excel->getActiveSheet()->getStyle('B:L')->getNumberFormat()->setFormatCode(\PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_NUMBER_00);
+
+
+//Alineacion
+//$excel->getActiveSheet() ->getStyle('A:L')->setQuotePrefix(true);
+$excel->getActiveSheet()->getStyle('A1:L1')->getAlignment()->setWrapText(true)->setVertical(true);
+
+$excel->getActiveSheet()->getStyle('A2:L2')
+  ->getAlignment()->setVertical(\PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_TOP);
+
+$excel->getActiveSheet()->getStyle('A1:L1')->ApplyFromArray($encabezados);
+//$excel->getActiveSheet()->getStyle('A4:L4')->ApplyFromArray($bordes);
+
+$hojaActiva->getColumnDimension('A')->setWidth(10); //Anchura de la celda
+$hojaActiva->setCellValue('A1', 'PERIODO'); //Titulo de la columna
+$hojaActiva->getColumnDimension('B')->setWidth(19);
+$hojaActiva->setCellValue('B1', 'CLAVE UNIDAD ADMINISTRATIVA');
+$hojaActiva->getColumnDimension('C')->setWidth(15);
+$hojaActiva->setCellValue('C1', 'CLAVE SERVIDOR PUBLICO');
+$hojaActiva->getColumnDimension('D')->setWidth(50);
+$hojaActiva->setCellValue('D1', 'NOMBRE');
+$hojaActiva->getColumnDimension('E')->setWidth(20);
+$hojaActiva->setCellValue('E1', 'RFC');
+$hojaActiva->getColumnDimension('F')->setWidth(13);
+$hojaActiva->setCellValue('F1', '0202 SUELDOS EVENTUALES');
+$hojaActiva->getColumnDimension('G')->setWidth(11);
+$hojaActiva->setCellValue('G1', '0325 SUBSIDIO AL EMPLEO');
+$hojaActiva->getColumnDimension('H')->setWidth(12);
+$hojaActiva->setCellValue('H1', '5408 IMPUESTO SOBRE LA RENTA');
+$hojaActiva->getColumnDimension('I')->setWidth(12);
+$hojaActiva->setCellValue('I1', '5540 SERVICIOS DE SALUD');
+$hojaActiva->getColumnDimension('J')->setWidth(11);
+$hojaActiva->setCellValue('J1', '5541 SIS. SOLIDARIO DE REPARTO');
+$hojaActiva->getColumnDimension('K')->setWidth(13);
+$hojaActiva->setCellValue('K1', '5542 SIS. CAPITAL. INDIVIDUAL');
+$hojaActiva->getColumnDimension('L')->setWidth(10);
+$hojaActiva->setCellValue('L1', 'TOTAL NETO');
 
 //Indicar que se comience desde la fila 2 de Excel y no reescribir los encanezados
 $fila = 2;
+$borde = 1;
 
 //Ciclo para leer el contenido de la consulta
 while ($rows = $resultado7->fetch_assoc()) {
-    //Extrayendo campos de la BD y especificando la columna donde se mostrara el contenido
-    $hojaActiva->setCellValue('A' . $fila, $rows['CvePersonal']);
-    $hojaActiva->setCellValue('B' . $fila, $rows['RFC']);
-    $hojaActiva->setCellValue('C' . $fila, $rows['Paterno']);
-    $hojaActiva->setCellValue('D' . $fila, $rows['Materno']);
-    $hojaActiva->setCellValue('E' . $fila, $rows['Nombre']);
-    $fila++;//Incrementando las filas en 1 para que se inserten apropiadamente
+  //Extrayendo campos de la BD y especificando la columna donde se mostrara el contenido
+  $hojaActiva->setCellValue('A' . $fila, $rows['Periodo']);
+  $hojaActiva->setCellValue('B' . $fila, $rows['ClaveUniAds']);
+  $hojaActiva->setCellValue('C' . $fila, $rows['CvePersonal']);
+  $hojaActiva->setCellValue('D' . $fila, $rows['Nombre']);
+  $hojaActiva->setCellValue('E' . $fila, $rows['Rfc']);
+  $hojaActiva->setCellValue('F' . $fila, $rows['SuelEven']);
+  $hojaActiva->setCellValue('G' . $fila, $rows['SubsEmp']);
+  $hojaActiva->setCellValue('H' . $fila, $rows['Isr']);
+  $hojaActiva->setCellValue('I' . $fila, $rows['ServSalud']);
+  $hojaActiva->setCellValue('J' . $fila, $rows['SisSolRep']);
+  $hojaActiva->setCellValue('K' . $fila, $rows['SisCapInd']);
+  $hojaActiva->setCellValue('L' . $fila, $rows['TotNet']);
+  //Incrementando las filas en 1 para que se inserten apropiadamente
+  $fila++;
+  //Incrementando las filas en 1 para que el borde se pinte segun el numero de columnas
+  $borde++;
+  //Indicando las celdas en las que se generara el borde vertical
+  $excel->getActiveSheet()->getStyle("A$borde:M$borde")->ApplyFromArray($bordesH);
+  $excel->getActiveSheet()->getStyle("A$borde:L$borde")->ApplyFromArray($bordesV);
 }
 
 //Creando el archivo de excel
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-header('Content-Disposition: attachment;filename="Reporte_Empleados.xlsx"');
+header('Content-Disposition: attachment;filename="SerPub'.$CveNomina.'.xlsx"');
 header('Cache-Control: max-age=0');
 
 //Indicando la salida del documento en xlsx 
 $writer = IOFactory::createWriter($excel, 'Xlsx');
 $writer->save('php://output');
 exit;
+?>
+
+
+
+
 
