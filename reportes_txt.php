c57e734067bcfe863f6e82a3dde26519247fb019
luisjuarezsanchez <luisjuarezingreso@hotmail.com>
2022-08-23 16:34:30 -0500

    Modificaciones al txt Maestro (Verificar Ing)

--- reportes_txt.php
+++ reportes_txt.php
@@ -36,80 +36,96 @@ $resultado3 = $mysqli->query($consulta3);
 
 //Ciclo para lectura y escritura del archivo
 while ($row = $resultado3->fetch_assoc()) {
-    //Numero consecutivo
-    //Ancho alto,borde,salto de linea justificacion relleno
-    fwrite($file, $row['CveEmp'] . '|');
-    fwrite($file, $row['Rfc'] . '|');
-    fwrite($file, $row['NomEmp'] . '|');
-    fwrite($file, $row['CveUniAds'] . '|');
-    fwrite($file, $row['CodCat'] . '|');
-    fwrite($file, $row['TipNom'] . '|');
-    fwrite($file, $row['CveIsse'] . '|');
-    fwrite($file, $row['Curp'] . '|');
-    fwrite($file, $row['NumCon'] . '|');
-    fwrite($file, $row['TotPer'] . '|');
-    fwrite($file, $row['TotDed'] . '|');
-    fwrite($file, $row['TotNet'] . '|');
-    fwrite($file, $row['TotDes'] . '|');
-    fwrite($file, $row['Qna'] . '|');
-    fwrite($file, $row['FecPag'] . '|'); //No la esta mandando
-    fwrite($file, $row['Fecini'] . '|');
-    fwrite($file, $row['FecFin'] . '|');
-    fwrite($file, $row['NumChe'] . '|');
-    fwrite($file, $row['CveOrg'] . '|');
-    fwrite($file, $row['OriRec'] . '|');
-    fwrite($file, $row['CveBan'] . '|');
-    fwrite($file, $row['Cuenta'] . '|');
-    fwrite($file, $row['FecIniCon'] . '|');
+  //Numero consecutivo
+  //Ancho alto,borde,salto de linea justificacion relleno
+  fwrite($file, $row['CveEmp'] . '|');
+  fwrite($file, $row['Rfc'] . '|');
+  fwrite($file, $row['NomEmp'] . '|');
+  fwrite($file, $row['CveUniAds'] . '|');
+  fwrite($file, $row['CodCat'] . '|');
+  fwrite($file, $row['TipNom'] . '|');
+  fwrite($file, $row['CveIsse'] . '|');
+  fwrite($file, $row['Curp'] . '|');
+  fwrite($file, $row['NumCon'] . '|');
+  fwrite($file, $row['TotPer'] . '|');
+  fwrite($file, $row['TotDed'] . '|');
+  fwrite($file, $row['TotNet'] . '|');
+  fwrite($file, ' ' . $row['TotDes'] . '|');
+  fwrite($file, $row['Qna'] . '|');
+  fwrite($file, $row['FecPag'] . '|'); //No la esta mandando
+  fwrite($file, $row['Fecini'] . '|');
+  fwrite($file, $row['FecFin'] . '|');
+  fwrite($file, $row['NumChe'] . '|');
+  fwrite($file, $row['CveOrg'] . '|');
+  fwrite($file, $row['OriRec'] . '|');
+  fwrite($file, $row['CveBan'] . '|');
+  fwrite($file, $row['Cuenta'] . '|');
+  fwrite($file, $row['FecIniCon'] . '|');
 
-    //Obteniendo fechas para calcular la antiguedad
-    $fecha1 = $row['FecIniCon'];
-    $fecha2 = $row['FecFin'];
-    //Almacenando fechas en variables 
-    $fechainicial = new DateTime($fecha1);
-    $fechafin = new DateTime($fecha2);
-    //Obteniendo la diferencia y almacenando en variables los datos
-    $diferencia = $fechainicial->diff($fechafin);
-    $anio = $diferencia->format('%y');
-    $mes = $diferencia->format('%m');
-    $dia = $diferencia->format('%d');
+  //Obteniendo fechas para calcular la antiguedad
+  $fecha1 = $row['FecIniCon'];
+  $fecha2 = $row['FecFin'];
+  //Almacenando fechas en variables 
+  $fechainicial = new DateTime($fecha1);
+  $fechafin = new DateTime($fecha2);
+  //Obteniendo la diferencia y almacenando en variables los datos
+  $diferencia = $fechainicial->diff($fechafin);
+  $anio = $diferencia->format('%y');
+  $mes = $diferencia->format('%m');
+  $dia = $diferencia->format('%d');
 
-    //Definiendo un array para imprimir los datos
-    $array =  array();
-    $array[0] = 'P';
+  //Definiendo un array para imprimir los datos
+  $array =  array();
+  $array[0] = 'P';
 
-    //Eliminando de la impresion los datos que sean 0
-    if ($anio > 0) {
-        $array[1] = $anio . "Y";
-    } else {
-        $array[1] = '';
-    }
+  //Eliminando de la impresion los datos que sean 0
+  if ($anio > 0) {
+    $array[1] = $anio . "Y";
+  } else {
+    $array[1] = '';
+  }
 
-    if ($mes > 0) {
-        $array[2] = $mes . "M";;
-    } else {
-        $array[2] = '';
-    }
+  if ($mes > 0) {
+    $array[2] = $mes . "M";;
+  } else {
+    $array[2] = '';
+  }
 
-    if ($dia > 0) {
-        $array[3] = ($dia + 1) . "D";
-    } else {
-        $array[3] = '';
-    }
-    //Imprimiendo las posiciones de los arreglos
-    fwrite($file, $array[0]);
-    fwrite($file, $array[1]);
-    fwrite($file, $array[2]);
-    fwrite($file, $array[3] . '|');
+  if ($dia > 0) {
+    $array[3] = ($dia + 1) . "D";
+  } else {
+    $array[3] = '';
+  }
+  //Imprimiendo las posiciones de los arreglos
+  fwrite($file, $array[0]);
+  fwrite($file, $array[1]);
+  fwrite($file, $array[2]);
+  fwrite($file, $array[3] . '|');
 
-    //Se continua con la impresion de la BD
-    fwrite($file, $row['Riesgo'] . '|');
-    fwrite($file, $row['SalDiaInt'] . '|'); //Falta
-    fwrite($file, $row['TipCont'] . '|');
+  //Se continua con la impresion de la BD
+  fwrite($file, $row['Riesgo'] . '|');
+  fwrite($file, $row['SalDiaInt'] . '|');
+  fwrite($file, $row['TipCont'] . '|');
+  //Eliminando el .00 de los resultados que sean igual a 0
+  if ($row['Subent'] > 0) {
     fwrite($file, $row['Subent'] . '|');
+  } else {
+    fwrite($file, '0' . '|');
+  }
+
+  if ($row['SubCau'] > 0) {
     fwrite($file, $row['SubCau'] . '|');
+  } else {
+    fwrite($file, '0' . '|');
+  }
+
+  if ($row['AjusSub'] > 0) {
     fwrite($file, $row['AjusSub']);
-    fwrite($file, '' . PHP_EOL);
+  } else {
+    fwrite($file, '0');
+  }
+
+  fwrite($file, '' . PHP_EOL);
 }
 
 //Cerrando el archivo
@@ -133,15 +149,15 @@ $resultado6 = $mysqli->query($consulta6);
 
 while ($row = $resultado6->fetch_assoc()) {
 
-    for ($i = 0; $i <= $row['cuenta']; $i++) {
-        //Llamando al procedimiento que inserta los campos en tmptidet
-        $clave = $row['clave'];
-        $consulta7 = "UPDATE tmptimdet SET NumCon=$num WHERE CveEmp=$clave";
+  for ($i = 0; $i <= $row['cuenta']; $i++) {
+    //Llamando al procedimiento que inserta los campos en tmptidet
+    $clave = $row['clave'];
+    $consulta7 = "UPDATE tmptimdet SET NumCon=$num WHERE CveEmp=$clave";
 
-        //jecutando update a NumCon
-        $resultado7 = $mysqli->query($consulta7);
-    }
-    $num = $num + 1;
+    //jecutando update a NumCon
+    $resultado7 = $mysqli->query($consulta7);
+  }
+  $num = $num + 1;
 }
 
 //Ejecutando consulta a tmptimmaes
@@ -152,15 +168,15 @@ $file2 = fopen("C:/Users/luisj/Desktop/DETALLE.txt", "w");
 
 
 while ($row = $resultado5->fetch_assoc()) {
-    fwrite($file2, $row['NumCon'] . '|');
-    fwrite($file2, $row['CveEmp'] . '|');
-    fwrite($file2, ltrim($row['CvePerDed'], "0") . '|');
-    fwrite($file2, $row['Monto'] . '|');
-    fwrite($file2, $row['Descri'] . '|');
-    fwrite($file2, $row['Cvesat'] . '|');
-    fwrite($file2, $row['Qna'] . '|');
-    fwrite($file2, $row['CveOrg']);
-    fwrite($file2, '' . PHP_EOL);
+  fwrite($file2, $row['NumCon'] . '|');
+  fwrite($file2, $row['CveEmp'] . '|');
+  fwrite($file2, ltrim($row['CvePerDed'], "0") . '|');
+  fwrite($file2, $row['Monto'] . '|');
+  fwrite($file2, $row['Descri'] . '|');
+  fwrite($file2, $row['Cvesat'] . '|');
+  fwrite($file2, $row['Qna'] . '|');
+  fwrite($file2, $row['CveOrg']);
+  fwrite($file2, '' . PHP_EOL);
 }
 fclose($file2);
 
@@ -187,7 +203,7 @@ $encabezados = [
       'rgb' => '538ED5'
     ]
   ]
-  
+
 ];
 
 
@@ -296,16 +312,10 @@ while ($rows = $resultado7->fetch_assoc()) {
 
 //Creando el archivo de excel
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-header('Content-Disposition: attachment;filename="SerPub'.$CveNomina.'.xlsx"');
+header('Content-Disposition: attachment;filename="SerPub' . $CveNomina . '.xlsx"');
 header('Cache-Control: max-age=0');
 
 //Indicando la salida del documento en xlsx 
 $writer = IOFactory::createWriter($excel, 'Xlsx');
 $writer->save('php://output');
 exit;
-?>
-
-
-
-
-
